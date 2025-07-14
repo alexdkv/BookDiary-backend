7a562707faf80a911b1ed1ead073145f6a6551c4
alexdkv <alexdekov120@gmail.com>
2025-07-14 12:22:06 +0300

    Initial backend initialization

---
+++ src/main/java/com/example/BookDiaryy/BookDiaryyApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookDiaryy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookDiaryyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookDiaryyApplication.class, args);
+	}
+
+}
